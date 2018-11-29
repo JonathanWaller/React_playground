d4819ecd1967773e3a3cf1c3263112c19f3daa5c
JonathanWaller <waller.j27@gmail.com>
2018-11-29 16:49:43 -0600

    testing git

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>This is the playground.</p>
+          <p>Let's test this again </p>
           <a
             className="App-link"
             href="https://reactjs.org"
